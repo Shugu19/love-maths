93150e691f261741df01659c88327b7803093dcb
Shubham badhan <shubhambadhan.uk@gmail.com>
2023-11-19 12:50:20 +0000

    Add eventListners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,38 @@
-function runGame(){
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.getElementsByTagName('button');
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            if (this.getAttribute('data-type') === 'submit') {
+                alert("You Clicked Submit!");
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`You Clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
+function runGame() {
 
 }
-function checkAnswer(){
+function checkAnswer() {
 
 }
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
 }
-function incrementScore(){
+function incrementScore() {
 
-}function incrementWrongAnswer(){
-
-}
-function displayAdditionQuestion(){
+} function incrementWrongAnswer() {
 
 }
-function displaySubtractQuestion(){
+function displayAdditionQuestion() {
 
 }
-function displayMultiplyQuestion(){
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
 
 }
